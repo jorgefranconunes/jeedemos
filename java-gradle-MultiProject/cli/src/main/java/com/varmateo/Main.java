d3f9a760b89e39e74b38f5927478b4b8920be2f6
jorgefranconunes <jorgefranconunes@gmail.com>
2017-11-05 16:30:53 +0100

    Refs #5 Added sample Gradle project with multiple sub-projects

---
+++ java-gradle-MultiProject/cli/src/main/java/com/varmateo/Main.java
@@ -0,0 +1,14 @@
+package com.varmateo;
+
+import com.varmateo.Greeter;
+
+
+public final class Main {
+
+
+    public static void main(final String[] args) {
+
+        Greeter.say("Hello, world!");
+    }
+
+}
