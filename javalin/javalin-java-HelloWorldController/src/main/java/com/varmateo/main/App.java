b83ca45553d838d0ffa882b4083d2258ae233b1f
jorgefranconunes <jorgefranconunes@gmail.com>
2018-04-10 18:29:27 +0200

    Refs #13 Started work on example for REST web service using Javalin

---
+++ javalin/javalin-java-HelloWorldController/src/main/java/com/varmateo/main/App.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package com.varmateo.main;
+
+
+public final class App {
+
+
+    public App() {
+        // Nothing to do.
+    }
+
+
+    public void start() {
+        // TBD
+    }
+
+}
